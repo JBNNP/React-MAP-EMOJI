3c9d68abfe388f5c915066cfa44c8f42d6ed61bf
johnbriancapate <johnbriancapate@gmail.com>
2022-12-08 01:12:03 +0800

    fix bug in dictionary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,10 @@ function App() {
        <h1>
         <span>emojipedia</span>
       </h1>
+      <dl className="dictionary">
       {emojipedia.map(createEntry)}
+      </dl>
+      
     </div>
   );
 }
