fa33d588c80c774ed1d9f10c5c3ac8f590f01509
johnbriancapate <johnbriancapate@gmail.com>
2022-12-07 17:33:59 +0800

    First Version

---
+++ src/components/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+
+function createEntry(props){
+  return (
+    <Entry emoji={props.emoji} 
+    name={props.name}
+    meaning={props.meaning}
+    />
+  )
+}
+
+
+function App() {
+  return (
+    <div>
+       <h1>
+        <span>emojipedia</span>
+      </h1>
+      {emojipedia.map(createEntry)}
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
